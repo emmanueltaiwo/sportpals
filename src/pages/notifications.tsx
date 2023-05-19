5f469dc158bceb8e8e1b1939af04375d09203170
Emmanuel Taiwo <devemmanuel1@gmail.com>
2023-05-19 09:58:46 +0100

    chore:optimized and modified notifications page

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -3,32 +3,50 @@ import Layout from "@/components/Layout";
 import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
-import Image from "next/image";
+import CheckIcon from "@mui/icons-material/Check";
+
+interface Notification {
+  id: number;
+  title: string;
+  description: string;
+  time: string;
+}
+
+const notifications: Notification[] = [
+  {
+    id: 1,
+    title: "Notification 1",
+    description: "Welcome to SportPals",
+    time: "Just Now",
+  },
+  {
+    id: 2,
+    title: "Notification 2",
+    description: "This is the second notification.",
+    time: "Just Now",
+  },
+  {
+    id: 3,
+    title: "Notification 3",
+    description: "This is the third notification.",
+    time: "Just Now",
+  },
+];
 
 const Notifications = () => {
   const router = useRouter();
-  const notifications = [
-    {
-      id: 1,
-      message: "Welcome to SportPals",
-      date: "10:00 AM",
-      read: false,
-    },
-    {
-      id: 2,
-      message: "Connect with your SportPal Today",
-      date: "11:30 AM",
-      read: true,
-    },
-    {
-      id: 3,
-      message: "Upgrade to Premium Today and enjoy all premium benefits",
-      date: "2:45 PM",
-      read: false,
-    },
-  ];
+  const [user, loading] = useAuthState(auth);
+   const [notificationList, setNotificationList] = useState<Notification[]>([]);
 
-  const [user, loading, error] = useAuthState(auth);
+   useEffect(() => {
+     setNotificationList(notifications);
+   }, []);
+
+   const removeNotification = (id: number) => {
+     setNotificationList((prevNotifications) =>
+       prevNotifications.filter((notification) => notification.id !== id)
+     );
+   };
 
   useEffect(() => {
     if (!loading && !user) {
@@ -46,26 +64,44 @@ const Notifications = () => {
           <span className="text-white font-bold">{notifications.length}</span>{" "}
           Notifications
         </p>
-        <div className="flex flex-col w-[100%] gap-8 h-fit bg-slate-800 py-10 rounded-t-3xl mt-8 ">
-          {notifications.map((item) => (
-            <div
-              key={item.id}
-              className="flex gap-3 bg-gray-600 w-[95%] px-2 py-2 mx-3 rounded-3xl transition-all duration-200"
-            >
-              <div className="w-[30%]">
-                <Image
-                  src={user?.photoURL || ""}
-                  width={60}
-                  height={60}
-                  className="rounded-lg"
-                  alt={""}
-                />
-              </div>
-              <div className="my-auto w-full">
-                <p className="text-sm text-gray-300">{item.message}</p>
-              </div>
-            </div>
-          ))}
+        <div className="flex flex-col w-[95%] gap-8 h-fit mx-auto rounded-t-3xl mt-8 ">
+          {notificationList.length > 0 ? (
+            <ul className="space-y-4">
+              {notificationList.map((notification) => (
+                <li
+                  key={notification.id}
+                  className="bg-white p-4 rounded-md shadow-md flex space-x-4"
+                >
+                  <div className="flex-shrink-0">
+                    <img
+                      src="/assets/images/logo_1.png"
+                      alt="User Avatar"
+                      className="w-12 h-12 rounded-full"
+                    />
+                  </div>
+                  <div className="flex-grow">
+                    <h3 className="text-base font-semibold mb-1">
+                      {notification.title}
+                    </h3>
+                    <p className="text-sm text-gray-500">
+                      {notification.description}
+                    </p>
+                    <p className="text-xs text-gray-400">{notification.time}</p>
+                  </div>
+                  <div className="flex items-center">
+                    <button
+                      className="text-sm text-blue-500 focus:outline-none"
+                      onClick={() => removeNotification(notification.id)}
+                    >
+                      <CheckIcon className="w-4 h-4" />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-gray-500">No notifications found.</p>
+          )}
         </div>
       </div>
     </Layout>
